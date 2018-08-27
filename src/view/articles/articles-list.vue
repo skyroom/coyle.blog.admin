03871725251a71c7e35364cc9c92499cd99b9a0b
skycity <283362006@qq.com>
2018-08-27 22:25:44 +0800

    add

--- src/view/articles/articles-list.vue
+++ src/view/articles/articles-list.vue
@@ -7,142 +7,145 @@
 
 <script>
 import {
-    mapMutations,
-    mapActions
-} from 'vuex';
+  mapMutations,
+  mapActions
+} from 'vuex'
 
 const columns = [{
+  title: '文章标题',
+  key: 'title'
+},
+{
+  title: '文章简介',
+  key: 'short'
+},
+{
+  title: '创建时间',
+  key: 'createdAt'
+},
+{
+  title: '操作',
+  key: 'action',
+  width: 150,
+  align: 'center',
+  render: (h, params) => {
+    return h('div', [
+      h('Button', {
+        props: {
+          type: 'primary',
+          size: 'small'
+        },
+        style: {
+          marginRight: '5px'
+        },
+        on: {
+          click: () => {
+            this.show(params.index)
+          }
+        }
+      }, '编辑'),
+      h('Button', {
+        props: {
+          type: 'error',
+          size: 'small'
+        },
+        on: {
+          click: () => {
+            this.remove(params)
+          }
+        }
+      }, '删除')
+    ])
+  }
+}
+]
+
+export default {
+  name: 'articlesList',
+  data () {
+    return {
+      loading: true,
+      columns: [{
         title: '文章标题',
-        key: 'title',
-    },
-    {
+        key: 'title'
+      },
+      {
         title: '文章简介',
         key: 'short'
-    },
-    {
+      },
+      {
         title: 'Action',
         key: 'action',
         width: 150,
         align: 'center',
         render: (h, params) => {
-            console.log('this is ', this);
-            return h('div', [
-                h('Button', {
-                    props: {
-                        type: 'primary',
-                        size: 'small'
-                    },
-                    style: {
-                        marginRight: '5px'
-                    },
-                    on: {
-                        click: () => {
-                            this.show(params.index)
-                        }
-                    }
-                }, '编辑'),
-                h('Button', {
-                    props: {
-                        type: 'error',
-                        size: 'small'
-                    },
-                    on: {
-                        click: () => {
-                            this.remove(params)
-                        }
-                    }
-                }, '删除')
-            ]);
-        }
-    }
-];
-
-export default {
-    name: 'articlesList',
-    data() {
-        return {
-            loading: true,
-            columns: [{
-                    title: '文章标题',
-                    key: 'title',
-                },
-                {
-                    title: '文章简介',
-                    key: 'short'
-                },
-                {
-                    title: 'Action',
-                    key: 'action',
-                    width: 150,
-                    align: 'center',
-                    render: (h, params) => {
-                        console.log('this is ', this);
-                        return h('div', [
-                            h('Button', {
-                                props: {
-                                    type: 'primary',
-                                    size: 'small'
-                                },
-                                style: {
-                                    marginRight: '5px'
-                                },
-                                on: {
-                                    click: () => {
-                                        this.show(params.index)
-                                    }
-                                }
-                            }, '编辑'),
-                            h('Button', {
-                                props: {
-                                    type: 'error',
-                                    size: 'small'
-                                },
-                                on: {
-                                    click: () => {
-                                        this.remove(params)
-                                    }
-                                }
-                            }, '删除')
-                        ]);
-                    }
+          console.log('this is ', this)
+          return h('div', [
+            h('Button', {
+              props: {
+                type: 'primary',
+                size: 'small'
+              },
+              style: {
+                marginRight: '5px'
+              },
+              on: {
+                click: () => {
+                  this.show(params.index)
                 }
-            ],
+              }
+            }, '编辑'),
+            h('Button', {
+              props: {
+                type: 'error',
+                size: 'small'
+              },
+              on: {
+                click: () => {
+                  this.remove(params)
+                }
+              }
+            }, '删除')
+          ])
         }
-    },
-    computed: {
-        articleList() {
-            return this.$store.state.article.articleList;
-        },
-    },
-    methods: {
-        ...mapActions([
-            'getArticleList'
-        ]),
-        addArticleHandle() {
-            this.$router.push('/article/add');
-        },
-        remove() {
-            this.$Modal.confirm({
-                title: '确定要删除此篇文章吗？',
-                loading: true,
-                onOk: () => {
-                    console.log('点击确认');
-                },
-            });
-        },
-        getArticleListHandle() {
-            this.getArticleList()
-            .then(() => {
-                this.$set(this, 'loading', false);
-            })
-            .catch((err) => {
-                this.$Notice.error({ title: err.msg || err });
-            });
-        },
-    },
-    mounted() {
-        this.getArticleListHandle();
+      }
+      ]
     }
+  },
+  computed: {
+    articleList () {
+      return this.$store.state.article.articleList
+    }
+  },
+  methods: {
+    ...mapActions([
+      'getArticleList'
+    ]),
+    addArticleHandle () {
+      this.$router.push('/article/add')
+    },
+    remove () {
+      this.$Modal.confirm({
+        title: '确定要删除此篇文章吗？',
+        loading: true,
+        onOk: () => {
+          console.log('点击确认')
+        }
+      })
+    },
+    getArticleListHandle () {
+      this.getArticleList()
+        .then(() => {
+          this.$set(this, 'loading', false)
+        })
+        .catch((err) => {
+          this.$Notice.error({ title: err.msg || err })
+        })
+    }
+  },
+  mounted () {
+    this.getArticleListHandle()
+  }
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const fs = require('fs')
 
 const resolve = dir => {
-    return path.join(__dirname, dir)
+  return path.join(__dirname, dir)
 }
 
 const env = process.env.NODE_ENV || 'development'
@@ -19,41 +19,42 @@ fs.writeFileSync(path.join(__dirname, './config/env.js'), `export default '${env
 const BASE_URL = '/'
 
 module.exports = {
-    // Project deployment base
-    // By default we assume your app will be deployed at the root of a domain,
-    // e.g. https://www.my-app.com/
-    // If your app is deployed at a sub-path, you will need to specify that
-    // sub-path here. For example, if your app is deployed at
-    // https://www.foobar.com/my-app/
-    // then change this to '/my-app/'
-    baseUrl: BASE_URL,
-    // tweak internal webpack configuration.
-    // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-    chainWebpack: config => {
-        config.resolve.alias.set('@', resolve('src')). // key,value自行定义，比如.set('@@', resolve('src/components'))
-        set('_c', resolve('src/components')).set('_conf', resolve('config'))
-    },
-    // 打包时不生成.map文件
-    productionSourceMap: false,
+  // Project deployment base
+  // By default we assume your app will be deployed at the root of a domain,
+  // e.g. https://www.my-app.com/
+  // If your app is deployed at a sub-path, you will need to specify that
+  // sub-path here. For example, if your app is deployed at
+  // https://www.foobar.com/my-app/
+  // then change this to '/my-app/'
+  baseUrl: BASE_URL,
+  // tweak internal webpack configuration.
+  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  chainWebpack: config => {
+    config.resolve.alias.set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('_c', resolve('src/components')).set('_conf', resolve('config'))
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false,
 
-    devServer: {
-        // host: '192.168.54.107',
-        port: 8071,
-        // https: false,
-        // hotOnly: true,
-        open: false,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3001',
-                ws: true,
-                changeOrigin: true,
-                pathRewrite: {
-                    '/api': '' // remove base path
-                },
-            },
-            // '/foo': {
-            //     target: '<other_url>'
-            // }
+  devServer: {
+    // host: '192.168.54.107',
+    port: 8071,
+    // https: false,
+    // hotOnly: true,
+    open: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': '' // remove base path
         }
+      }
+      // '/foo': {
+      //     target: '<other_url>'
+      // }
     }
+  },
+  lintOnSave: false
 }
